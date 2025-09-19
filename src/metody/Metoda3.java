3d5375d4b4029de7638877ba48d94ebc70682c42
Przemek Janisz <p.janisz@ckp-ns.edu.pl>
2025-09-19 10:36:20 +0200

    metody

---
+++ src/metody/Metoda3.java
@@ -0,0 +1,16 @@
+package metody;
+
+public class Metoda3 {
+    public static void main(String[] args) {
+        int liczba1 =10, liczba2 = 7;
+        int suma;
+
+        suma = dodaj(liczba1,liczba2);
+
+        System.out.println("Suma wynosi: " + suma);
+    }
+    public static int dodaj(int a, int b){
+        int wynik = a+b;
+        return wynik;
+    }
+}
